ef156946c472d2fd0b3a222c33bf3a5f84ad2669
Pa Her <pa.her@gofmx.com>
2022-05-03 21:40:02 -0500

    Add Producers view folder and index view

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -6,8 +6,11 @@ namespace eTickets.Models
     {
         [Key]
         public int ID { get; set; }
+        [Display(Name = "Profile Picture URL")]
         public string? ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
         public string? FullName { get; set; }
+        [Display(Name = "Biography")]
         public string? Bio { get; set; }
 
         public List<Movie>? Movies { get; set; }
